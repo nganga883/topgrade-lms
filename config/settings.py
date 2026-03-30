8d0e9fa39315bd97dcd474071979b421755fbd96
Nicholas Mwangi <ngangamwangi99@gmail.com>
2026-03-30 14:39:47 +0300

    fix

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-2re@)u1*$()*dmnaynlvcr@1*ta!$&416xun*x%%a&!&qy8&bu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [] [
+    "topgrade-lms-1.onrender.com",
+    "localhost"
+    "127.0.0.1"
+]
 
 
 # Application definition
