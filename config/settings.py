56ce0e7a14d19b3f657789fa80bfc65edf86188a
Nicholas Mwangi <ngangamwangi99@gmail.com>
2026-03-31 02:33:54 +0300

    fixed database and settings

--- config/settings.py
+++ config/settings.py
@@ -86,8 +86,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 import os
 import dj_database_url
+import os
+import dj_database_url
+import dj_database_url
+
 DATABASES = {
-    'default': dj_database_url.parse(os.getenv("DATABASE_URL"))
+    'default': dj_database_url.config(
+        default=f"sqlite:///{os.path.join(BASE_DIR, 'db.sqlite3')}"
+    )
 }
 
 
