d1b28a7622ccbc6ddc60e242a18da6f8080c5b83
Nicholas Mwangi <ngangamwangi99@gmail.com>
2026-03-31 01:12:39 +0300

    fix

--- config/settings.py
+++ config/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -82,6 +84,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
+import os
 import dj_database_url
 DATABASES = {
     'default': dj_database_url.parse(os.getenv("DATABASE_URL"))
